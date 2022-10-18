084acb1923e05f587bee85b02fbd4763d41f9347
Austin <kennedystampsnc28709@gmail.com>
2022-10-18 16:38:06 -0400

    finished hour updater function

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -2,20 +2,35 @@ $(document).ready(function () {
     // listen for save button clicks
     $('.saveBtn').on('click', function () {
         // get nearby values
-        var text = $('div.textarea').text();
+        var text = $(this).closest(".textarea").text
         // save in localStorage
-        localStorage.setItem('div.textarea', text);
+        localStorage.setItem("text", )
         // Show notification that item was saved to localStorage by adding class 'show'
         $('#notify').show();
         // Timeout to remove 'show' class after 5 seconds
         setTimeout('$("#notify").hide()',5000);
-        
     });
-
+    
     function hourUpdater() {
         // get current number of hours (preferably with moment.js)
-
+        currentHour = moment().hour();
         // loop over time blocks
+        $('.time-block').each(function () {
+            var blockHour = parseInt($(this).attr('id').split('-')[1]) ;
+
+            if (blockHour < currentHour) {
+                $(this).addClass('past');
+            }  else if (blockHour === CurrentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            }  else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        })
+
+        
         // loop over time blocks ---> https://api.jquery.com/each/
         // inside this loop, // check if we've moved past this time. If we have, make the row grey. If it's future, make it green. if it's past, make it red. Using the past, present, and future classes in css file
 
