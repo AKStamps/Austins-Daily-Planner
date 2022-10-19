501c6b92d6cd5649ba011c7a0a1fc098294bc0cd
Austin <kennedystampsnc28709@gmail.com>
2022-10-18 22:00:54 -0400

    appended local storage to text areas

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     // listen for save button clicks
     $('.saveBtn').on('click', function () {
-        event.preventDefault();
         // get nearby values
         var time = $(this).parent().attr('id');
         var content = $(this).siblings('.description').val();
@@ -47,15 +46,18 @@ $(document).ready(function () {
     var loadPrev = [
     // load any saved data from localStorage
     $('#hour-9 .description').val(localStorage.getItem('hour-9')),
-    $('#hour-9 .description').val(localStorage.getItem('hour-10')),
-    $('#hour-9 .description').val(localStorage.getItem('hour-11')),
-    $('#hour-9 .description').val(localStorage.getItem('hour-12')),
-    $('#hour-9 .description').val(localStorage.getItem('hour-13')),
-    $('#hour-9 .description').val(localStorage.getItem('hour-14')),
-    $('#hour-9 .description').val(localStorage.getItem('hour-15')),
-    $('#hour-9 .description').val(localStorage.getItem('hour-16')),
-    $('#hour-9 .description').val(localStorage.getItem('hour-17'))
+    $('#hour-10 .description').val(localStorage.getItem('hour-10')),
+    $('#hour-11 .description').val(localStorage.getItem('hour-11')),
+    $('#hour-12 .description').val(localStorage.getItem('hour-12')),
+    $('#hour-13 .description').val(localStorage.getItem('hour-13')),
+    $('#hour-14 .description').val(localStorage.getItem('hour-14')),
+    $('#hour-15 .description').val(localStorage.getItem('hour-15')),
+    $('#hour-16 .description').val(localStorage.getItem('hour-16')),
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'))
     ];
+
+    $(loadPrev).append;
+   
 
     
     ///need to repeat line 21 for all the other hours
